a81b91b37c8b2f4ab57e2d3255a509a0033183b9
xiezhenhao <xiezhenhao@gmail.com>
2015-04-12 23:35:16 +0800

    init
    for nieheng's graduate project

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebControlNie.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "家居监控Web控制端";
+
+            return View();
+        }
+
+        public ActionResult Security()
+        {
+            ViewBag.Message = "照明";
+
+            return View();
+        }
+
+        public ActionResult Light()
+        {
+            ViewBag.Message = "照明";
+
+            return View();
+        }
+
+        public ActionResult Swith()
+        {
+            ViewBag.Message = "开关";
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebControlNie.MvcApplication" Language="C#" %>
